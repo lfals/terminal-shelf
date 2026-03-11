7fccac6ad8132e55a9cbf19a5524af34c34821c5
Luis Amorim <luisfelipeamorim@hotmail.com>
2026-03-11 19:43:57 -0300

    base

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 const geistSans = Geist({
   variable: "--font-sans",
@@ -23,12 +25,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
+    <TooltipProvider>
+    <html lang="pt-BR" className={cn("font-sans", geistSans.variable)}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} min-h-screen antialiased`}
-      >
+        >
         {children}
       </body>
     </html>
+        </TooltipProvider>
   );
 }
